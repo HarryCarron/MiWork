d891feffb7c5570fbe0f6fdece303458fb5adc47
Harry Carron <hcarron92@gmail.com>
2019-03-24 11:27:44 +0000

    Issue #9 STM offset issue fixed
    caclulating scroll ammount by adding container heights of all months leading to selected

--- src/app/calendars/calendar/calendar.component.ts
+++ src/app/calendars/calendar/calendar.component.ts
@@ -18,50 +18,21 @@ export class CalendarComponent implements OnInit {
 
     selectedMonth: number = null;
 
-
-    // public scrollToMonth (month: number): void {
-    //     const thisMonth: string = MonthsOfYear[month].toLowerCase();
-    //     $('.innerCalendarContainer').animate(
-    //         { scrollTop: $('#scroll-' + thisMonth).offset().top - 100 }
-    //         , 1000);
-
-    //         this.selectedMonth = month + 1;
-    //         console.log($('#scroll-' + thisMonth).offset().top - 100 );
-    // }
-
-
     public scrollToMonth (month: number): void {
 
-        const thisMonth: string = MonthsOfYear[month].toLowerCase();
-
-        const calContainerHeight = $('#scroll-january').height();
-
+        let scrollDistance = 0;
+        for (let i = 1; i < month; i++) {
+            scrollDistance = scrollDistance + $('#scroll-' + MonthsOfYear[i].toLowerCase()).height();
+        }
         $('.innerCalendarContainer').animate(
-            { scrollTop: calContainerHeight * (month - 1) }
+            { scrollTop: scrollDistance }
             , 1000);
-
             this.selectedMonth = month + 1;
-            console.log($('#scroll-' + thisMonth).offset().top - 100 );
     }
-
-
 
     ngOnInit() {
         setTimeout(() => {
             this.scrollToMonth(new Date().getMonth() + 1);
-
-        Array
-         .from({length: 12})
-         .map((j, i) =>  i + 1 )
-         .forEach((k, u) => {
-            console.log(
-                'offset: ' + $('#scroll-' + MonthsOfYear[k].toLowerCase()).offset().top
-                );
-                console.log(
-                    'position: ' + $('#scroll-' + MonthsOfYear[k].toLowerCase()).position().top
-                    );
-        });
-
         }, 400);
 
     }
